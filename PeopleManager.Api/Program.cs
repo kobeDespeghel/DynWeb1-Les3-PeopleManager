e89ab3aecd87b913639a53d5d5fa22069d45a4a4
despeghelkobe <kobe.despeghel@student.vives.be>
2025-09-29 12:25:54 +0200

    added swagger

--- PeopleManager.Api/Program.cs
+++ PeopleManager.Api/Program.cs
@@ -9,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
 
 //var connectionString = builder.Configuration.GetConnectionString(nameof(PeopleManagerDbContext));
 
@@ -27,6 +28,8 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
